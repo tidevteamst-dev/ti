f1033c7cb68c551fa91870b40289416e1200ea79
tidevteamst-dev <tidevteam.st@gmail.com>
2025-08-07 15:36:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from django.core.wsgi import get_wsgi_application
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ti.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ti_web.settings")
 application = get_wsgi_application()
+
